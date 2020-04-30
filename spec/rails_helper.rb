bda4ba8322ed9dd5157ccd898ed9853fe2bde65a
James Tinker <j.tinker30@hotmail.com>
2020-04-30 17:13:33 -0400

    moved codecov to proper file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,3 @@
-# Code coverage
-require 'simplecov'
-SimpleCov.start
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
