a828e9875b2de322490eddc6ac894484ac3cf2c2
James Tinker <j.tinker30@hotmail.com>
2020-04-30 18:35:15 -0400

    fixed testing bugs

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Member, type: :model do
   # Association test
-  it { should belong_to(:shirt_size) }
-  it { should belong_to(:short_size) }
+  it { should belong_to(:shirt_size).class_name('Size').optional }
+  it { should belong_to(:short_size).class_name('Size').optional }
+  it { should belong_to(:referral_method).optional }
   it { should belong_to(:status) }
   it { should have_many(:team_members) }
   it { should have_many(:teams) }
-  it { shuold have_many(:member_positions) }
+  it { should have_many(:member_positions) }
   it { should have_many(:positions) }
   # Validation tests
   # ensure columns are present before saving
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:birthday) }
-  it { shuold validate_presence_of(:postal_code) }
+  it { should validate_presence_of(:postal_code) }
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Team, type: :model do
   # Association test
   it { should belong_to(:sponsor) }
-  it { should belong_to(:convenor) }
+  it { should belong_to(:convenor).class_name('Member').optional }
   it { should belong_to(:division) }
   it { should have_many(:team_members) }
   it { should have_many(:members) }
