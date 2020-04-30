0889d9c28b10247a7c61191682260ae9c7f2a90a
James Tinker <j.tinker30@hotmail.com>
2020-04-30 19:22:01 -0400

    moved require

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,8 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.include RequestSpecHelper
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
@@ -61,8 +63,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.include RequestSpecHelper
 
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
