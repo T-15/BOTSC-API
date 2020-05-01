cafdcefc4ba41545acb148d39c635964ea406c97
James Tinker <j.tinker30@hotmail.com>
2020-04-30 22:02:55 -0400

    modified params to make them match expected response

--- spec/requests/api/v1/constitution_articles_request_spec.rb
+++ spec/requests/api/v1/constitution_articles_request_spec.rb
@@ -70,7 +70,7 @@ RSpec.describe "Api::V1::ConstitutionArticles", type: :request do
         end
 
         context 'when the request is invalid' do
-            before { post '/api/v1/constitution_articles', params: { title: 'Foobar' }.to_json }
+            before { post '/api/v1/constitution_articles', params: { { title: 'Foobar' } }.to_json }
 
             it 'returns status code 422' do
                 expect(response).to have_http_status(422)
@@ -78,7 +78,7 @@ RSpec.describe "Api::V1::ConstitutionArticles", type: :request do
 
             it 'returns a validation failure message' do
                 expect(response.body)
-                .to match("\"content\":[\"can't be blank\"]")
+                .to match("{\"content\":[\"can't be blank\"]}")
             end
         end
     end
@@ -100,8 +100,8 @@ RSpec.describe "Api::V1::ConstitutionArticles", type: :request do
     describe 'DELETE /api/v1/constitution_articles/:id' do
         before { delete "/api/v1/constitution_articles/#{constitution_article_id}", params: {} }
 
-        it 'returns status code 200' do
-        expect(response).to have_http_status(200)
+        it 'returns status code 204' do
+        expect(response).to have_http_status(204)
         end
     end
 
