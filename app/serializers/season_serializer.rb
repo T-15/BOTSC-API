a853980835b0060fbc740b5f16568437c2022810
James Tinker <j.tinker30@hotmail.com>
2020-05-03 13:42:12 -0400

    Fixed test fails

--- app/serializers/season_serializer.rb
+++ app/serializers/season_serializer.rb
@@ -1,6 +1,6 @@
 class SeasonSerializer < ActiveModel::Serializer
   # attributes to be serialized  
-  attributes :id, :year, :name, schedule_generated
+  attributes :id, :year, :name, :schedule_generated
   # model association
   has_many :divisions
 end
